fdcc54b0ce6b89184ee69f9a98b87a12a065b038
Christoph Pech <christoph.pech@gmail.com>
2024-06-16 14:06:19 +0200

    Update prerequisites.sh

--- bms/prerequisites.sh
+++ bms/prerequisites.sh
@@ -1,8 +1,10 @@
+/opt/victronenergy/swupdate-scripts/resize2fs.sh
+
 opkg update
 opkg install python3
 opkg install python3-pip
 pip3 install --upgrade pip
-pip3 install python3-dev
+pip3 install python-dev
 pip3 install binutils packagegroup-core-buildessential
 pip3 install python-can
 
