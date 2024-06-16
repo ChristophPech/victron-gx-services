51cabac7e91e330e63576cabf3965756d2df86f7
Christoph Pech <christoph.pech@gmail.com>
2024-06-16 13:41:07 +0200

    Update prerequisites.sh

--- bms/prerequisites.sh
+++ bms/prerequisites.sh
@@ -1,4 +1,8 @@
 opkg update
+opkg install python3
 opkg install python3-pip
+pip3 install --upgrade pip
+pip3 install python-dev
+pip3 install binutils packagegroup-core-buildessential
 pip3 install python-can
 
